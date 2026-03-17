34626679bf47d630ee4e961115115cbb43eaede2
Saba_Zakeri_Far <szakerifar@my.bcit.ca>
2026-03-17 15:25:12 -0700

    apply the exact same logic for favoriting in eventpage in explore

--- src/explore.js
+++ src/explore.js
@@ -1,20 +1,23 @@
 import { db } from "./firebaseConfig.js";
-import { collection, doc, getDocs, onSnapshot } from "firebase/firestore";
+import { auth } from "./firebaseConfig.js";
+import { collection, doc, getDocs, getDoc, setDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+// same logic that i used in eventspage for favoriting cards for logged in user will be used here
+
 async function loadevents() {
   const ref = collection(db, "Events_2026");
   const snap = await getDocs(ref);
   let populate = 0;
 
-  snap.forEach((doc) => {
-    let data = doc.data();
-    let id = doc.id ;
+  snap.forEach((eventdoc) => {
+    let data = eventdoc.data();
+    let id = eventdoc.id ;
       let result = `<div class="explorecards flex flex-col lg:flex-row lg:w-1/2  w-full gap-5  bg-white rounded-xl  shadow-md  mb-4  items-stretch lg:min-h-[300px] ">
-    
+
     <div class="lg:w-1/2 w-full h-auto relative">
-      <img 
-        id="evntimglist" 
-        class="w-full h-full object-cover rounded-l-xl" 
-        src="./images/${doc.id}.png"" 
+      <img
+        id="evntimglist"
+        class="w-full h-full object-cover rounded-l-xl"
+        src="./images/${eventdoc.id}.png"
         alt="Event"
       >
       <div class="flex  flex-row flex-wrap gap-2 pb-4 absolute bottom-0 left-1  ">
@@ -39,75 +42,167 @@ async function loadevents() {
     <div class="p-6 lg:w-1/2 flex flex-col justify-center">
       <div class="flex flex-col">
 <h2 class="evnttitle text-xl font-bold text-[#445629] mb-2">${data.title}</h2>
- 
+
       </div>
       <p class="text-gray-700   text-sm pb-4 evntdesclist" > ${data.shortest_summary}
-        
+
       </p>
 
       <!-- tags for the event -->
 
-        <a href="eventpage.html?docID=${doc.id}&from=explore.html" class="w-fit bg-black text-white text-sm py-1 px-3 rounded-lg hover:opacity-90 transition-opacity">
+        <a href="eventpage.html?docID=${eventdoc.id}&from=explore.html" class="w-fit bg-black text-white text-sm py-1 px-3 rounded-lg hover:opacity-90 transition-opacity">
         Go to Event
-      </a> 
+      </a>
     </div>
     <!-- more cards -->
   </div>`;
    const card = document.createElement("div");
     card.innerHTML = result;
+
+
     if (populate < 2) {
-      // favorite button
-    let favselected = false;
 
-document.querySelectorAll(".favbtn").forEach(function(btn) {
-    btn.addEventListener("click", function(){favClick(btn);})});
-
-function favClick(x) {
-    if (favselected == false) {
-        x.style.fill = "red";
-        x.style.stroke = "none";
-        favselected = true;
-    } else {
-        x.style.fill = "black";
-        x.style.stroke = "black";
-        favselected = false;
-    }
-    return x;
-}
-      document.querySelector(".firstcontainer").appendChild(card.firstElementChild);
+      const each_card = document.querySelector(".firstcontainer").appendChild(card.firstElementChild);
+      const favBtn = each_card.querySelector(".favbtn");
+      const user = auth.currentUser;
+      if (user) {
+        async function check_fav_field(){
+          const ref = await getDoc(doc(db, "users", user.uid))
+          const user_data = ref.data()
+          if (!user_data.favorite_events){
+            let saved_events = []
+            await setDoc(doc(db, "users", user.uid), {
+              favorite_events: saved_events,
+            },{ merge: true })
+            return saved_events
+          } else {
+            return user_data.favorite_events
+          }
+        }
+        async function remember_heart_color(x){
+          await check_fav_field();
+          const ref = await getDoc(doc(db, "users", user.uid));
+          const ref_data_event = ref.data().favorite_events;
+          if (ref_data_event.includes(eventdoc.id)){
+            x.style.fill = "red";
+            x.style.stroke = "none";
+          } else {
+            x.style.fill = "black";
+            x.style.stroke = "black";
+          }
+        }
+        remember_heart_color(favBtn);
+        favBtn.addEventListener("click", function () {
+          favClick(favBtn);
+        });
+        async function favClick(x) {
+          await check_fav_field();
+          let favselected = false;
+          const ref = await getDoc(doc(db, "users", user.uid));
+          const ref_data = ref.data();
+          if (!ref_data.favorite_events.includes(eventdoc.id)){
+            favselected = false;
+          } else {
+            favselected = true;
+          }
+          if (favselected == false) {
+            x.style.fill = "red";
+            x.style.stroke = "none";
+            favselected = true;
+            const id_fav = eventdoc.id
+            await updateDoc(doc(db, "users", user.uid), {
+              favorite_events : arrayUnion(id_fav),
+            })
+          } else {
+            favselected = false;
+            x.style.fill = "black";
+            x.style.stroke = "black";
+            const id_fav = eventdoc.id;
+            await updateDoc(doc(db, "users", user.uid), {
+              favorite_events : arrayRemove(id_fav),
+            })
+          }
+          return x;
+        }
+      }
       populate++;
-    } 
-    else if (populate >= 2 && populate < 4){
-            // favorite button
-    let favselected = false;
-
-document.querySelectorAll(".favbtn").forEach(function(btn) {
-    btn.addEventListener("click", function(){favClick(btn);})});
-
-function favClick(x) {
-    if (favselected == false) {
-        x.style.fill = "red";
-        x.style.stroke = "none";
-        favselected = true;
-    } else {
-        x.style.fill = "black";
-        x.style.stroke = "black";
-        favselected = false;
     }
-    return x;
-}
-    document.querySelector(".secondcontainer").appendChild(card.firstElementChild);
-    populate++;
+    else if (populate >= 2 && populate < 4){
+
+      const each_card =document.querySelector(".secondcontainer").appendChild(card.firstElementChild);
+      const favBtn = each_card.querySelector(".favbtn");
+      const user = auth.currentUser;
+      if (user) {
+        async function check_fav_field(){
+          const ref = await getDoc(doc(db, "users", user.uid))
+          const user_data = ref.data()
+          if (!user_data.favorite_events){
+            let saved_events = []
+            await setDoc(doc(db, "users", user.uid), {
+              favorite_events: saved_events,
+            },{ merge: true })
+            return saved_events
+          } else {
+            return user_data.favorite_events
+          }
+        }
+        async function remember_heart_color(x){
+          await check_fav_field();
+          const ref = await getDoc(doc(db, "users", user.uid));
+          const ref_data_event = ref.data().favorite_events;
+          if (ref_data_event.includes(eventdoc.id)){
+            x.style.fill = "red";
+            x.style.stroke = "none";
+          } else {
+            x.style.fill = "black";
+            x.style.stroke = "black";
+          }
+        }
+        remember_heart_color(favBtn);
+        favBtn.addEventListener("click", function () {
+          favClick(favBtn);
+        });
+        async function favClick(x) {
+          await check_fav_field();
+          let favselected = false;
+          const ref = await getDoc(doc(db, "users", user.uid));
+          const ref_data = ref.data();
+          if (!ref_data.favorite_events.includes(eventdoc.id)){
+            favselected = false;
+          } else {
+            favselected = true;
+          }
+          if (favselected == false) {
+            x.style.fill = "red";
+            x.style.stroke = "none";
+            favselected = true;
+            const id_fav = eventdoc.id
+            await updateDoc(doc(db, "users", user.uid), {
+              favorite_events : arrayUnion(id_fav),
+            })
+          } else {
+            favselected = false;
+            x.style.fill = "black";
+            x.style.stroke = "black";
+            const id_fav = eventdoc.id;
+            await updateDoc(doc(db, "users", user.uid), {
+              favorite_events : arrayRemove(id_fav),
+            })
+          }
+          return x;
+        }
+      }
+      populate++;
 
     }
     else if (populate >= 4 && populate <= 6){
 
         let new_result = ` <div class="w-full lg:w-1/3 p-2">
-          
+
           <div class="flex flex-col rounded-2xl  h-full shadow-md border-0 bg-white ">
             <div class="w-full relative">
               <div class="relative">
-                <img class="h-48 w-full object-cover rounded-t-xl " src="./images/${doc.id}.png" alt="">
+                <img class="h-48 w-full object-cover rounded-t-xl " src="./images/${eventdoc.id}.png" alt="">
                 <div class="flex  flex-row gap-2 pb-4 absolute bottom-0 left-1  ">
         <span class=" bg-[var(--light-blue)] w-fit flex-nowrap text-white py-1 px-2 rounded-full text-xs">
            <span id="evnttag">${data.tags[0]}</span>
@@ -117,7 +212,7 @@ function favClick(x) {
         </span>
       </div>
               </div>
-                  
+
               <div >
                     <button class="rounded-full w-fit h-fit bg-white absolute top-3 right-3 p-2">
                       <svg class="favbtn fill-black w-6 h-6 stroke-black stroke-2" id="" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
@@ -131,10 +226,10 @@ function favClick(x) {
           </div>
             <div class="p-4">
               <h3 id="evntdate" class="font-semibold text-sm  text-[var(--medium-grey)] pt-2">${data.date}</h3>
-               
+
             <p id="evnttitle" class=" font-bold text-md pt-3 pb-8">${data.title}</p>
-            <a href="eventpage.html?docID=${doc.id}&from=eventlist.html" 
-              type="button"  
+            <a href="eventpage.html?docID=${eventdoc.id}&from=eventlist.html"
+              type="button"
               class="bg-black rounded-xl text-white text-center  mt-8 px-2 py-2 text-xs cursor-pointer focus:outline-none">
               View Details
             </a>
@@ -142,27 +237,71 @@ function favClick(x) {
           </div>
           </div>`
 
-                // favorite button
-    let favselected = false;
-
-document.querySelectorAll(".favbtn").forEach(function(btn) {
-    btn.addEventListener("click", function(){favClick(btn);})});
-
-function favClick(x) {
-    if (favselected == false) {
-        x.style.fill = "red";
-        x.style.stroke = "none";
-        favselected = true;
-    } else {
-        x.style.fill = "black";
-        x.style.stroke = "black";
-        favselected = false;
-    }
-    return x;
-}
           let new_card = document.createElement("div")
           new_card.innerHTML = new_result
-          document.querySelector(".thirdcontainer").appendChild(new_card.firstElementChild);
+          const card = document.querySelector(".thirdcontainer").appendChild(new_card.firstElementChild);
+          const favBtn = card.querySelector(".favbtn");
+          const user = auth.currentUser;
+          if (user) {
+            async function check_fav_field(){
+              const ref = await getDoc(doc(db, "users", user.uid))
+              const user_data = ref.data()
+              if (!user_data.favorite_events){
+                let saved_events = []
+                await setDoc(doc(db, "users", user.uid), {
+                  favorite_events: saved_events,
+                },{ merge: true })
+                return saved_events
+              } else {
+                return user_data.favorite_events
+              }
+            }
+            async function remember_heart_color(x){
+              await check_fav_field();
+              const ref = await getDoc(doc(db, "users", user.uid));
+              const ref_data_event = ref.data().favorite_events;
+              if (ref_data_event.includes(eventdoc.id)){
+                x.style.fill = "red";
+                x.style.stroke = "none";
+              } else {
+                x.style.fill = "black";
+                x.style.stroke = "black";
+              }
+            }
+            remember_heart_color(favBtn);
+            favBtn.addEventListener("click", function () {
+              favClick(favBtn);
+            });
+            async function favClick(x) {
+              await check_fav_field();
+              let favselected = false;
+              const ref = await getDoc(doc(db, "users", user.uid));
+              const ref_data = ref.data();
+              if (!ref_data.favorite_events.includes(eventdoc.id)){
+                favselected = false;
+              } else {
+                favselected = true;
+              }
+              if (favselected == false) {
+                x.style.fill = "red";
+                x.style.stroke = "none";
+                favselected = true;
+                const id_fav = eventdoc.id
+                await updateDoc(doc(db, "users", user.uid), {
+                  favorite_events : arrayUnion(id_fav),
+                })
+              } else {
+                favselected = false;
+                x.style.fill = "black";
+                x.style.stroke = "black";
+                const id_fav = eventdoc.id;
+                await updateDoc(doc(db, "users", user.uid), {
+                  favorite_events : arrayRemove(id_fav),
+                })
+              }
+              return x;
+            }
+          }
           populate++;
     }
   });
